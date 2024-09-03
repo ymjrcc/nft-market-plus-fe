47212b1e22fbf54bbdef9032bf5193d940794e44
heyiming <ymjrcc@qq.com>
2024-09-03 16:11:25 +0800

    add nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Button } from '@nextui-org/button'; 
 
 function Page() {
   return (
@@ -15,6 +16,7 @@ function Page() {
       <h1 className="text-3xl font-bold underline text-blue-600 text-center p-8">
         Hello Tailwindcss!
       </h1>
+      <Button>Click me</Button>
     </>
   );
 }
